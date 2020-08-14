ee5211cf566eb5e18f27e4078f80712717a2412a
Tomek <kemot15@tlen.pl>
2020-08-14 11:20:18 +0200

    admin panel

--- Moustique/Models/Enums/Pages.cs
+++ Moustique/Models/Enums/Pages.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Moustique.Models.Enums
 {
     public enum Pages
     {
+        [Display(Name = "Home")]
         Home,
+        [Display(Name = "Usługi")]
         Service,
+        [Display(Name = "O nas")]
         About,
+        [Display(Name = "Cennik")]
         Price,
+        [Display(Name = "Kontakt")]
         Contact
     }
 }
